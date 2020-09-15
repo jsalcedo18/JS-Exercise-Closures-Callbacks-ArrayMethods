e7ecfec8c7f64941c9105239e49025c1028cbc37
Jaynee Salcedo <jsalcedoeducation@gmail.com>
2020-09-14 16:11:18 -1000

    Task 1 Completed

--- index.js
+++ index.js
@@ -27,31 +27,40 @@ function processFirstItem(stringList, callback) {
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
- * 
+ * 		The difference between counter 1 and counter 2, is counter 1 has a function inside of a function; so counter 1 will be returning 
+ *    counterMaker by counting up from 0. Now counter 2 is its own function
+ 
  * 2. Which of the two uses a closure? How can you tell?
- * 
+ *    Of the 2, counter 1 uses a closure because function counter reaches out of its scope to counterMaker, to understand that it is counting 
+ *    from 0 (let count = 0;) which is inside of counterMakers function. While counter 2 is its own function
+ 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
+     counter 1 would be preferable when you need a funtion to calculate something, in order to provide information for the parent function 
+     to create information that would make an option to use it later along the code; verus just making it independent to that one function. 
+     For example; you need the child function to calculate how much wood One yard needs. That child function is then used by the parent 
+     function whose determining how much wood a whole block needs. Now if you were doing even bigger calculations for a whole neighborhood, 
+     you would be able to reference back to that function. Now for counter 2, its an independent function that would just count an item from 0; 
+     and all its information needed is the item its counting up for; it doesn't need any additional.
  *
 */
 
 // counter1 code
 function counterMaker() {
   let count = 0;
-  return function counter() {
-    count++;
-  }
-}
+      return function counter() {
+        count++; //Counting up from 0
+        }
+}//Function CounterMaker End
 
 const counter1 = counterMaker();
-
 // counter2 code
-let count = 0;
+  let count = 0;
 
 function counter2() {
   return count++;
-}
+}//Function Counter 2 End
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /* Task 2: inning() 
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
